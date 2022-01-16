d45f058c438fd8567961af0f825928884b32ff82
abc <abc@DESKTOP-3MPBEL5>
2022-01-16 20:53:42 +0300

    Добавьте файлы проекта.

---
+++ Trushkova_HW_Base1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+while (true)
+{
+
+
+    double SideFromClient(int num)
+    {
+        try
+        {
+            Console.WriteLine($"Введите {num}-ю сторону прямоугольника:");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        catch (Exception ex)
+        {
+            Show(ex.Message);
+        }
+    }
+
+    void Show(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+}
---
+++ Trushkova_HW_Base1/Rectangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Rectangle
+    {
+        private double side1, side2;        
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = CheckforZero(side1);
+            this.side2 = CheckforZero(side2);
+        }
+
+        double Side1
+        {
+            get { return this.side1; }
+            set { this.side1 = CheckforZero((double)value); }
+        }
+
+        double Side2
+        {
+            get { return this.side2; }
+            set { this.side2 = CheckforZero((double)value); }
+        }
+
+        double Area
+        {
+            get { return CheckforZero(AreaCalculator()); }
+        }
+
+        double Perimeter
+        {
+            get
+            { return CheckforZero(PerimeterCalculator()); }
+        }
+
+        double AreaCalculator()
+            => side1 * side2;
+
+        double PerimeterCalculator()
+            => (side1 + side2) * 2;
+
+        double CheckforZero(double num)
+        {
+            if (num != 0)
+            {
+                return num;
+            }
+            else
+            {
+                throw new Exception("Сторона прямоугольнка не может равняться 0");
+            }
+        }
+    }
+}
