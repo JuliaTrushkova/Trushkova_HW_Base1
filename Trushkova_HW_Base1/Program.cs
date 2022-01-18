c1fd360d0bd2bfb69b0d96cd1578a3f02718524a
Julia Trushkova <kosyuska@yandex.ru>
2022-01-18 20:03:34 +0300

    fix: fix bug of readline

--- Trushkova_HW_Base1/Program.cs
+++ Trushkova_HW_Base1/Program.cs
@@ -6,29 +6,39 @@ Console.WriteLine("Hello, World!");
 
 while (true)
 {
+    try
+    {
+        double side1 = SideFromClient(1);
+        double side2 = SideFromClient(2);
 
-    double side1 = SideFromClient(1);
-    double side2 = SideFromClient(2);
+        Rectangle rectangle = new Rectangle(side1, side2);
 
-    Rectangle rectangle = new Rectangle(side1, side2);
-
-    Console.WriteLine($"Периметр прямоугольника со сторонами {side1} и {side2} равен {rectangle.Perimeter}");
-    Console.WriteLine($"Площадь прямоугольника со сторонами {side1} и {side2} равна {rectangle.Area}");
-    Console.WriteLine();
+        Console.WriteLine($"Периметр прямоугольника со сторонами {side1} и {side2} равен {rectangle.Perimeter}");
+        Console.WriteLine($"Площадь прямоугольника со сторонами {side1} и {side2} равна {rectangle.Area}");
+        Console.WriteLine();
+    }
+    catch (Exception ex)
+    {
+        Show(ex.Message);
+    }
 }
 
 
 double SideFromClient(int num)
 {
     double side = 0;
-    try
-    {
-        Console.WriteLine($"Введите {num}-ю сторону прямоугольника:");
-        side = Convert.ToDouble(Console.ReadLine());
-    }
-    catch (Exception ex)
-    {
-        Show(ex.Message);
+    while (true)
+    {        
+        try
+        {
+            Console.WriteLine($"Введите {num}-ю сторону прямоугольника:");
+            side = Convert.ToDouble(Console.ReadLine());
+            break;
+        }
+        catch (Exception ex)
+        {
+            Show(ex.Message);
+        }        
     }
     return side;
 }
